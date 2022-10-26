543566460e2619dd0d32a947a77bca24fbe04b53
vankoosh <vankoosh@gmail.com>
2022-10-26 14:12:44 +0000

    imgs links fixed

--- src/components/Parent/Parent.jsx
+++ src/components/Parent/Parent.jsx
@@ -11,27 +11,27 @@ export default function Parent() {
     {
       id: "For Loop",
       count: "1/5",
-      src: require("https://github.com/vankoosh/codecademy-cheatsheet/blob/master/public/imgs/LOOPS/ForLoop.jpg"),
+      src: "imgs/LOOPS/ForLoop.jpg",
     },
     {
       id: "While Loop",
       count: "2/5",
-      src: require("https://github.com/vankoosh/codecademy-cheatsheet/blob/master/public/imgs/LOOPS/WhileLoop.jpg"),
+      src: "imgs/LOOPS/WhileLoop.jpg",
     },
     {
       id: "Do While Loop",
       count: "3/5",
-      src: "codecademy-cheatsheet/imgs/LOOPS/DoWhileLoop.jpg",
+      src: "imgs/LOOPS/DoWhileLoop.jpg",
     },
     {
       id: "Nested Loop",
       count: "4/5",
-      src: "codecademy-cheatsheet/imgs/LOOPS/NestedLoop-ComparingTwoArrays.jpg",
+      src: "imgs/LOOPS/NestedLoop-ComparingTwoArrays.jpg",
     },
     {
       id: "Break Keyword",
       count: "5/5",
-      src: "codecademy-cheatsheet/imgs/LOOPS/BreakKeyword.jpg",
+      src: "imgs/LOOPS/BreakKeyword.jpg",
     },
   ]);
 
@@ -73,27 +73,27 @@ export default function Parent() {
                 {
                   id: "For Loop",
                   count: "1/5",
-                  src: "codecademy-cheatsheet/imgs/LOOPS/ForLoop.jpg",
+                  src: "imgs/LOOPS/ForLoop.jpg",
                 },
                 {
                   id: "While Loop",
                   count: "2/5",
-                  src: "codecademy-cheatsheet/imgs/LOOPS/WhileLoop.jpg",
+                  src: "imgs/LOOPS/WhileLoop.jpg",
                 },
                 {
                   id: "Do While Loop",
                   count: "3/5",
-                  src: "codecademy-cheatsheet/imgs/LOOPS/DoWhileLoop.jpg",
+                  src: "imgs/LOOPS/DoWhileLoop.jpg",
                 },
                 {
                   id: "Nested Loop",
                   count: "4/5",
-                  src: "codecademy-cheatsheet/imgs/LOOPS/NestedLoop-ComparingTwoArrays.jpg",
+                  src: "imgs/LOOPS/NestedLoop-ComparingTwoArrays.jpg",
                 },
                 {
                   id: "Break Keyword",
                   count: "5/5",
-                  src: "codecademy-cheatsheet/imgs/LOOPS/BreakKeyword.jpg",
+                  src: "imgs/LOOPS/BreakKeyword.jpg",
                 },
               ]);
               underline(e);
@@ -111,22 +111,22 @@ export default function Parent() {
                 {
                   id: 1,
                   count: "1/4",
-                  src: "codecademy-cheatsheet/imgs/ARRAYS/Arrays.jpg",
+                  src: "imgs/ARRAYS/Arrays.jpg",
                 },
                 {
                   id: 2,
                   count: "2/4",
-                  src: "codecademy-cheatsheet/imgs/ARRAYS/ChangeArrayElementsByAssignment.jpg",
+                  src: "imgs/ARRAYS/ChangeArrayElementsByAssignment.jpg",
                 },
                 {
                   id: 3,
                   count: "3/4",
-                  src: "codecademy-cheatsheet/imgs/ARRAYS/AccesingIndexOfNestedArrays.jpg",
+                  src: "imgs/ARRAYS/AccesingIndexOfNestedArrays.jpg",
                 },
                 {
                   id: 4,
                   count: "4/4",
-                  src: "codecademy-cheatsheet/imgs/ARRAYS/NestedLoop-ComparingTwoArrays.jpg",
+                  src: "imgs/ARRAYS/NestedLoop-ComparingTwoArrays.jpg",
                 },
               ]);
               underline(e);
@@ -143,32 +143,32 @@ export default function Parent() {
                 {
                   id: 1,
                   count: "1/6",
-                  src: "/imgs/ARRAY-ITERATORS/filter().jpg",
+                  src: "imgs/ARRAY-ITERATORS/filter().jpg",
                 },
                 {
                   id: 2,
                   count: "2/6",
-                  src: "/imgs/ARRAY-ITERATORS/findIndex().jpg",
+                  src: "imgs/ARRAY-ITERATORS/findIndex().jpg",
                 },
                 {
                   id: 3,
                   count: "3/6",
-                  src: "/imgs/ARRAY-ITERATORS/forEach().jpg",
+                  src: "imgs/ARRAY-ITERATORS/forEach().jpg",
                 },
                 {
                   id: 4,
                   count: "4/6",
-                  src: "/imgs/ARRAY-ITERATORS/map().jpg",
+                  src: "imgs/ARRAY-ITERATORS/map().jpg",
                 },
                 {
                   id: 5,
                   count: "5/6",
-                  src: "/imgs/ARRAY-ITERATORS/reduce().jpg",
+                  src: "imgs/ARRAY-ITERATORS/reduce().jpg",
                 },
                 {
                   id: 6,
                   count: "6/6",
-                  src: "/imgs/ARRAY-ITERATORS/IteratorsExamples.jpg",
+                  src: "imgs/ARRAY-ITERATORS/IteratorsExamples.jpg",
                 },
               ]);
               underline(e);
@@ -185,27 +185,27 @@ export default function Parent() {
                 {
                   id: 1,
                   count: "1/5",
-                  src: "/imgs/CONDITIONALS/ConditionalStatementsReview.jpg",
+                  src: "imgs/CONDITIONALS/ConditionalStatementsReview.jpg",
                 },
                 {
                   id: 2,
                   count: "2/5",
-                  src: "/imgs/CONDITIONALS/SwitchStatement.jpg",
+                  src: "imgs/CONDITIONALS/SwitchStatement.jpg",
                 },
                 {
                   id: 3,
                   count: "3/5",
-                  src: "/imgs/CONDITIONALS/TernaryOperator.jpg",
+                  src: "imgs/CONDITIONALS/TernaryOperator.jpg",
                 },
                 {
                   id: 4,
                   count: "4/5",
-                  src: "/imgs/CONDITIONALS/TruthyAndFalsyStatements.jpg",
+                  src: "imgs/CONDITIONALS/TruthyAndFalsyStatements.jpg",
                 },
                 {
                   id: 5,
                   count: "5/5",
-                  src: "/imgs/CONDITIONALS/Short-circuitOREvaluation.jpg",
+                  src: "imgs/CONDITIONALS/Short-circuitOREvaluation.jpg",
                 },
               ]);
               underline(e);
@@ -222,67 +222,67 @@ export default function Parent() {
                 {
                   id: 1,
                   count: "1/13",
-                  src: "/imgs/OBJECTS/ObjectsReview.jpg",
+                  src: "imgs/OBJECTS/ObjectsReview.jpg",
                 },
                 {
                   id: 2,
                   count: "2/13",
-                  src: "/imgs/OBJECTS/AdvancedObjectsReview.jpg",
+                  src: "imgs/OBJECTS/AdvancedObjectsReview.jpg",
                 },
                 {
                   id: 3,
                   count: "3/13",
-                  src: "/imgs/OBJECTS/LoopingThroughObjects.jpg",
+                  src: "imgs/OBJECTS/LoopingThroughObjects.jpg",
                 },
                 {
                   id: 4,
                   count: "4/13",
-                  src: "/imgs/OBJECTS/CreatingObjectLiterals.jpg",
+                  src: "imgs/OBJECTS/CreatingObjectLiterals.jpg",
                 },
                 {
                   id: 5,
                   count: "5/13",
-                  src: "/imgs/OBJECTS/Destructured{}Assignment.jpg",
+                  src: "imgs/OBJECTS/Destructured{}Assignment.jpg",
                 },
                 {
                   id: 6,
                   count: "6/13",
-                  src: "/imgs/OBJECTS/Built-inMethodsLikeObject.keys().entries().assign().jpg",
+                  src: "imgs/OBJECTS/Built-inMethodsLikeObject.keys().entries().assign().jpg",
                 },
                 {
                   id: 7,
                   count: "7/13",
-                  src: "/imgs/OBJECTS/FactoryFunctions.jpg",
+                  src: "imgs/OBJECTS/FactoryFunctions.jpg",
                 },
                 {
                   id: 8,
                   count: "8/13",
-                  src: "/imgs/OBJECTS/ObjectsAndPrivacy.jpg",
+                  src: "imgs/OBJECTS/ObjectsAndPrivacy.jpg",
                 },
                 {
                   id: 9,
                   count: "9/13",
-                  src: "/imgs/OBJECTS/RefactoringKeyValueShorthand.jpg",
+                  src: "imgs/OBJECTS/RefactoringKeyValueShorthand.jpg",
                 },
                 {
                   id: 10,
                   count: "10/13",
-                  src: "/imgs/OBJECTS/Getters.jpg",
+                  src: "imgs/OBJECTS/Getters.jpg",
                 },
                 {
                   id: 11,
                   count: "11/13",
-                  src: "/imgs/OBJECTS/Setters.jpg",
+                  src: "imgs/OBJECTS/Setters.jpg",
                 },
                 {
                   id: 12,
                   count: "12/13",
-                  src: "/imgs/OBJECTS/thisKeyword.jpg",
+                  src: "imgs/OBJECTS/thisKeyword.jpg",
                 },
                 {
                   id: 13,
                   count: "13/13",
-                  src: "/imgs/OBJECTS/thisAndArrowFunctions.jpg",
+                  src: "imgs/OBJECTS/thisAndArrowFunctions.jpg",
                 },
               ]);
               underline(e);
@@ -297,26 +297,26 @@ export default function Parent() {
           <li
             onClick={(e) => {
               setTopic([
-                { id: "1/6", src: "/imgs/FUNCTIONS/FunctionDeclaration.jpg" },
+                { id: "1/6", src: "imgs/FUNCTIONS/FunctionDeclaration.jpg" },
                 {
                   id: "2/6",
-                  src: "/imgs/FUNCTIONS/FunctionExpression.jpg",
+                  src: "imgs/FUNCTIONS/FunctionExpression.jpg",
                 },
                 {
                   id: "3/6",
-                  src: "/imgs/FUNCTIONS/ConciseBodyArrowFunctions.jpg",
+                  src: "imgs/FUNCTIONS/ConciseBodyArrowFunctions.jpg",
                 },
                 {
                   id: "4/6",
-                  src: "/imgs/FUNCTIONS/DefaultParameter.jpg",
+                  src: "imgs/FUNCTIONS/DefaultParameter.jpg",
                 },
                 {
                   id: "5/6",
-                  src: "/imgs/FUNCTIONS/HelperFunctions.jpg",
+                  src: "imgs/FUNCTIONS/HelperFunctions.jpg",
                 },
                 {
                   id: "6/6",
-                  src: "/imgs/FUNCTIONS/ReturnKeyword.jpg",
+                  src: "imgs/FUNCTIONS/ReturnKeyword.jpg",
                 },
               ]);
               underline(e);
@@ -330,34 +330,34 @@ export default function Parent() {
           <li
             onClick={(e) => {
               setTopic([
-                { id: "1/8", src: "/imgs/CLASSES/Classes.jpg" },
+                { id: "1/8", src: "imgs/CLASSES/Classes.jpg" },
                 {
                   id: "2/8",
-                  src: "/imgs/CLASSES/Constructor.jpg",
+                  src: "imgs/CLASSES/Constructor.jpg",
                 },
                 {
                   id: "3/8",
-                  src: "/imgs/CLASSES/ANewInstance.jpg",
+                  src: "imgs/CLASSES/ANewInstance.jpg",
                 },
                 {
                   id: "4/8",
-                  src: "/imgs/CLASSES/AddingMethods.jpg",
+                  src: "imgs/CLASSES/AddingMethods.jpg",
                 },
                 {
                   id: "5/8",
-                  src: "/imgs/CLASSES/CallingMethods.jpg",
+                  src: "imgs/CLASSES/CallingMethods.jpg",
                 },
                 {
                   id: "6/8",
-                  src: "/imgs/CLASSES/StaticSuperclassMethod.jpg",
+                  src: "imgs/CLASSES/StaticSuperclassMethod.jpg",
                 },
                 {
                   id: "7/8",
-                  src: "/imgs/CLASSES/ExtendSubclassFromSuperclass.jpg",
+                  src: "imgs/CLASSES/ExtendSubclassFromSuperclass.jpg",
                 },
                 {
                   id: "8/8",
-                  src: "/imgs/CLASSES/ExcersiseWithDefaultParams.jpg",
+                  src: "imgs/CLASSES/ExcersiseWithDefaultParams.jpg",
                 },
               ]);
               underline(e);
@@ -373,47 +373,47 @@ export default function Parent() {
               setTopic([
                 {
                   id: "1/11",
-                  src: "/imgs/FETCH/fetch()Review.jpg",
+                  src: "imgs/FETCH/fetch()Review.jpg",
                 },
                 {
                   id: "2/11",
-                  src: "/imgs/FETCH/fetch()GETDiagram.jpg",
+                  src: "imgs/FETCH/fetch()GETDiagram.jpg",
                 },
                 {
                   id: "3/11",
-                  src: "/imgs/FETCH/fetch()GETExample.jpg",
+                  src: "imgs/FETCH/fetch()GETExample.jpg",
                 },
                 {
                   id: "4/11",
-                  src: "/imgs/FETCH/fetch()GETExampleWithPromise.jpg",
+                  src: "imgs/FETCH/fetch()GETExampleWithPromise.jpg",
                 },
                 {
                   id: "5/11",
-                  src: "/imgs/FETCH/fetch()POSTDiagram.jpg",
+                  src: "imgs/FETCH/fetch()POSTDiagram.jpg",
                 },
                 {
                   id: "6/11",
-                  src: "/imgs/FETCH/fetch()POSTExample.jpg",
+                  src: "imgs/FETCH/fetch()POSTExample.jpg",
                 },
                 {
                   id: "7/11",
-                  src: "/imgs/FETCH/fetch()POSTExample II.jpg",
+                  src: "imgs/FETCH/fetch()POSTExample II.jpg",
                 },
                 {
                   id: "8/11",
-                  src: "/imgs/FETCH/asyncAwaitGETwithFetch()Diagram.jpg",
+                  src: "imgs/FETCH/asyncAwaitGETwithFetch()Diagram.jpg",
                 },
                 {
                   id: "9/11",
-                  src: "/imgs/FETCH/asyncAwaitGETwithFetch()Example.jpg",
+                  src: "imgs/FETCH/asyncAwaitGETwithFetch()Example.jpg",
                 },
                 {
                   id: "10/11",
-                  src: "/imgs/FETCH/asyncAwaitPOSTwithFetch()Diagram.jpg",
+                  src: "imgs/FETCH/asyncAwaitPOSTwithFetch()Diagram.jpg",
                 },
                 {
                   id: "11/11",
-                  src: "/imgs/FETCH/asyncAwaitPOSTwithFetch()Example.jpg",
+                  src: "imgs/FETCH/asyncAwaitPOSTwithFetch()Example.jpg",
                 },
               ]);
               underline(e);
